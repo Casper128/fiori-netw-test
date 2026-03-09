10c866ea9aa9e71ea620aa326c36ff1cd487c40c
Casper128 <darwin.oso128@gmail.com>
2026-03-09 16:38:18 -0500

    feat: first commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("ZPRUEBA_01ZPRUEBA_01.controller.Main", {
+		console.log("Hola");
+	});
+});
